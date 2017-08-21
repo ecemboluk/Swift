a3a4560adb3ba2fa2edaab721732b4d492c42f2d
Ecem Bölük <ecemboluk@gmail.com>
2017-08-21 16:27:06 +0300

    Swift-Değişkenler
    Swift değişkenler ve veri tipleri videosunda anlattığım kodlar

---
+++ degiskenler.swift
@@ -0,0 +1,68 @@
+//
+//  main.swift
+//  Hello world
+//
+//  Created by Ecem on 21.08.2017.
+//  Copyright © 2017 Ecem. All rights reserved.
+//
+
+import Foundation
+
+var isim = "Ecem";
+var sayi = 5;
+var isBool = true;
+var deneme:Float = 14.3
+
+let program = "Swift"
+
+var string1 = "Birinci String";
+var string2 = "İkinci String";
+
+
+print(string1+string2)
+print(string1+string1)
+
+var c1 = "E"
+var c2 = "C"
+var c3 = "E"
+var c4 = "M"
+
+var c5:Character = "A"
+
+print(c1+c2+c3+c4)
+
+var sayi1:Int = 10;
+var sayi2:Int64 = 20;
+
+var sayi3:Int
+sayi3=39;
+
+print(sayi3)
+print(5)
+
+
+var ondalik1 = 23.9
+
+var isWork = true;
+isWork=false;
+if(!isWork){
+    print("Çalışmıyor")
+}
+
+var sayi6:Float = 23.3;
+var sayi7 = 15.3
+
+var sayi9:Float = sayi6 * Float(sayi7)
+
+print(sayi9)
+
+print("Sayi girin :")
+
+var yazi = readLine()
+
+print(yazi)
+
+
+
+
+
